b7d75cb7ff708f3b6d32384911d64629453829ac
gregbast1994 <gregbast1994@gmail.com>
2020-01-21 15:22:24 -0500

    redo script, improve OOP

--- script.js
+++ script.js
@@ -1,39 +1,67 @@
-// game
-const Player = ( symbol ) => {
-    let score = 0;
-    const getSymbol = () => symbol;
+const Board = ( ) => {
+    const board = [
+        '', '', '',
+        '', '', '',
+        '', '', '',
+    ]
+
+    const render = () => {
+        Array.from(document.getElementsByClassName('cell')).forEach(function(item, i) {
+            document.getElementById(item.id).innerText = board[i]
+        })
+
+        console.log( board )
+    }
+
+    const set = ( position, value ) => {
+        board[position - 1] = value
+    }
+
+    return { render, set }
 }
 
-const Board = () => {
-    let cells = [];
+const Player = ( symbol ) => {
+    let score = 0
+    const getSymbol = () => symbol
+
+    return { getSymbol, score }
 }
 
 const ticTacToe = (() => {
-    
-    // declarations
-    const board = Board();
-    const player1 = Player('X');
-    const player2 = Player('O');
+    let turn = 1
+    const board = Board()
 
-    let cellElems = document.getElementsByClassName('cell');
-
-    function playerMove( e ){
-        console.log( e )
+    const players = {
+        'X': Player('X'),
+        'O': Player('O')
     }
 
-    function gameOver(){
-        console.log( 'gameover' )
+    const whoseTurnIsIt = () => {
+        // X - first
+        // O - second
+        return ( turn % 2 ) ? 'O' : 'X'
     }
 
-    function restart(){
-        console.log( 'restart' )
+    const move = ( e ) => {
+        //remove 'cell-' from cell id
+        if( e.target.innerText === '' ){
+            let index = e.target.id.slice(5)
+            board.set(index, players[whoseTurnIsIt()].getSymbol() )
+            board.render()
+            turn++
+        }
     }
 
-    // ADD EVENT
-    for( let cell of cellElems ){
-        cell.addEventListener( 'click', function(e){
-            console.log( e )
-        } );
-    }
+    Array.from(document.getElementsByClassName('cell')).forEach(function(cell) {
+        cell.addEventListener('click', move)
+    })
 
-})();
+    return {
+        board, players
+    };
+
+  })();
+
+  console.log( ticTacToe.board )
+
+
